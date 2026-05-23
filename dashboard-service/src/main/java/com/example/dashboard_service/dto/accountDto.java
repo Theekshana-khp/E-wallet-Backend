803aa8a7024d1dc99e2a127bb3512b6299a8bb33
pasindutheekshana903@gmail.com <pasindutheekshana903@gmail.com>
2026-05-24 00:21:31 +0530

    further develop dashboard servises

---
+++ dashboard-service/src/main/java/com/example/dashboard_service/dto/accountDto.java
@@ -0,0 +1,24 @@
+package com.example.dashboard_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class accountDto {
+    private Long id;
+    private Long userId;
+    private String accountNumber;
+    private BigDecimal balance;
+    private String currency;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
