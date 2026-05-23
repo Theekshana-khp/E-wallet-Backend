b9f83a3f13e2bf9160c7d9096fbcb9ff01a46c99
pasindutheekshana903@gmail.com <pasindutheekshana903@gmail.com>
2026-05-24 00:21:55 +0530

    further develop dashboard servises

--- dashboard-service/src/main/java/com/example/dashboard_service/service/dashboardService.java
+++ dashboard-service/src/main/java/com/example/dashboard_service/service/dashboardService.java
@@ -1,7 +1,11 @@
 package com.example.dashboard_service.service;
 
+import com.example.dashboard_service.dto.NotificationDto;
+import com.example.dashboard_service.dto.accountDto;
 import com.example.dashboard_service.dto.dashboardDTO;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import com.example.dashboard_service.dto.transactionDTO;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -21,19 +25,51 @@ public class dashboardService {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public Mono<dashboardDTO> getDashboard() {
+    public Mono<dashboardDTO> getDashboard (String keyClockId , String token){
+        return webClient.get()
+                .uri("http://user-service/user/getId?keycloakId=" + keyClockId)
+                .header("Authorization", "Bearer " + token)
+                .retrieve()
+                .bodyToMono(Long.class)
+                .flatMap(user -> {
+                    Mono<List<transactionDTO>> transactions =
+                            webClient.get()
+                                    .uri("http://transaction-service/transaction")
+                                    .retrieve()
+                                    .bodyToFlux(transactionDTO.class)
+                                    .collectList();
+                    Mono<List<NotificationDto>> notifications =
+                            webClient.get()
+                                    .uri("http://notification-service/notification/user/{user}", user)
+                                    .retrieve()
+                                    .bodyToFlux(NotificationDto.class)
+                                    .collectList();
+                    Mono<accountDto> account =
+                            webClient.get()
+                                    .uri("http://account-service/accout/getAccountDetails?userId={user}",user)
+                                    .retrieve()
+                                    .bodyToMono(accountDto.class);
 
-        Mono<List<transactionDTO>> transactions =
-                webClient.get()
-                        .uri("http://transaction-service/transaction")
-                        .retrieve()
-                        .bodyToFlux(transactionDTO.class)
-                        .collectList();
 
-        return transactions
-                .map(list -> {
-                    kafkaTemplate.send("transaction-topic", "Load Transaction Success");
-                    return new dashboardDTO(list);
+                    return Mono.zip(transactions, notifications, account)
+                            .map(tuple -> {
+
+                                List<transactionDTO> transactionList = tuple.getT1();
+                                List<NotificationDto> notificationList = tuple.getT2();
+                                accountDto accountData = tuple.getT3();
+
+                                kafkaTemplate.send(
+                                        "transaction-topic",
+                                        "Load Transaction Success"
+                                );
+
+                                return new dashboardDTO(
+                                        transactionList,
+                                        notificationList,
+                                        accountData
+                                );
+                            });
                 });
+
     }
 }
